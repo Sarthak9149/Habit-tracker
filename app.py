63a53a42890abeeefebad2360a670d1cadcce363
Sarthak9149 <168849656+Sarthak9149@users.noreply.github.com>
2025-11-08 22:16:14 +0530

    login page completed

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
-# flask --app app.py --debug run
+from datetime import date
+
+# command to run the file - flask --app app.py --debug run
+
+# to get the current date time
+def get_current_date():
+    today = date.today()
+    return today.strftime("%B %d, %Y")
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", current_date = get_current_date())
 
 @app.route("/login")
 def login():
@@ -51,7 +59,26 @@ def success():
         return render_template("error.html", message=f"Database Error: {e}")
 
     return render_template("success.html")
-    
+
+
+@app.route("/loginsuccessfull", methods=["POST"])
+def login_completed():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # Check credentials in the database
+    with sqlite3.connect('users.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
+        user = cursor.fetchone()
+        
+
+        if user:
+            return render_template("index.html", name=user[1], current_date = get_current_date())  # Example: Redirect to user dashboard
+        else:
+            return render_template("error.html", message="Invalid email or password.")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
